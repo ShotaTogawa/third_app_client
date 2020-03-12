c024522fde4a887b623732cf3892d8bd8f38cc60
ShotaTogawa <s.togawa0112@gmail.com>
2020-03-12 14:14:41 -0700

    fixed code based on reviews

--- src/components/Like/Like.jsx
+++ src/components/Like/Like.jsx
@@ -5,37 +5,41 @@ import { api } from '../../api';
 const Like = ({ photoId }) => {
   const [isLike, setIsLike] = useState(false);
   const [countLike, setCountLike] = useState(0);
+  const [isDisabled, setIsDisabled] = useState(false);
   useEffect(() => {
     const fetchLike = async () => {
       const response = await api.get(`/api/like/${photoId}`);
-      setIsLike(response.data);
-    };
-    const fetchCountLike = async () => {
-      const response = await api.get(`/api/likes/${photoId}`);
-      if (response.data.length === 0) {
+      setIsLike(response.data[0]);
+      if (response.data[1].length === 0) {
         setCountLike(0);
       } else {
-        const count = response.data[0];
+        const count = response.data[1][0];
         setCountLike(count.likes);
       }
     };
     fetchLike();
-    fetchCountLike();
-  }, [photoId]);
+  }, []);
 
   const like = async e => {
     e.preventDefault();
-    await api.post(`/api/like/${photoId}`);
-    setCountLike(countLike + 1);
+    setIsDisabled(true);
+    const response = await api.post(`/api/like/${photoId}`);
+    setIsDisabled(false);
+    setCountLike(response.data[0].likes);
     setIsLike(true);
   };
 
   const unlike = async e => {
     e.preventDefault();
-    await api.delete(`/api/unlike/${photoId}`);
-    setCountLike(countLike - 1);
-
+    setIsDisabled(true);
+    const response = await api.delete(`/api/unlike/${photoId}`);
+    if (response.data.length === 0) {
+      setCountLike(0);
+    } else {
+      setCountLike(response.data[0].likes);
+    }
     setIsLike(false);
+    setIsDisabled(false);
   };
   return (
     <LikeWrapper>
@@ -44,9 +48,15 @@ const Like = ({ photoId }) => {
           className="fas fa-heart"
           onClick={e => unlike(e)}
           color={'red'}
+          disabled={isDisabled}
         />
       ) : (
-        <Heart className="fas fa-heart" onClick={e => like(e)} color={'#fff'} />
+        <Heart
+          className="fas fa-heart"
+          onClick={e => like(e)}
+          color={'#fff'}
+          disabled={isDisabled}
+        />
       )}
       <Count>{countLike ? countLike : 0}</Count>
     </LikeWrapper>
@@ -67,7 +77,10 @@ const Count = styled.p`
   margin-left: 0.5rem;
 `;
 
-const Heart = styled.i`
+const Heart = styled.button`
+  background-color: transparent;
+  border: none;
+  outline: none;
   margin-left: 1.5rem;
   font-size: 2rem;
   color: ${props => props.color};
